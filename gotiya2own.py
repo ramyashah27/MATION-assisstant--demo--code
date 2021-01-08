9a7c6c7b8e11fdeef316da8e4f286066f80d0077
ramyashah27 <75496156+ramyashah27@users.noreply.github.com>
2021-01-08 13:26:42 +0530

    Update gotiya2own.py

--- gotiya2own.py
+++ gotiya2own.py
@@ -5,9 +5,9 @@ import random
 import datetime
 from playsound import playsound
 import time
-import webbrowser
 import pyttsx3
 import cv2
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 
@@ -32,78 +32,55 @@ def wishme():
     speak("I am gotiya the OP bot how may i help U")
 if __name__ == "__main__":
     wishme()
-sr=s.Recognizer()
-print("HOW MAY I HELP U please TELL")
+    while True:
+        sr=s.Recognizer()
+        print("I AM LISTING,HOW MAY I HELP U please TELL")
+        with s.Microphone() as m:
+            audio=sr.listen(m)
+            query=sr.recognize_google(audio,language='eng-in')
+            print(query)
 
+        # if "browser" or "browser" in query:
+        #     webbrowser.open('https://www.google.com/')
+        # elif "YouTube" in query:
+        #     webbrowser.open('https://www.youtube.com')
+        # else:
+        #     speak("cant reach it")
+        #     print('cant reach it')
+        if 'Discord' in query:
+            print('opening discord for YOU')
+            speak('opening discord for YOU')
+            codepath= 'C:\\Users\\ADMIN\\Desktop\\ramya shah\\Discord.lnk'
+            os.startfile(codepath)
+        if "joke" in query:
+            cjoke=pyjokes.get_joke()
+            print(cjoke)
+            speak(cjoke)
+        if "time" in query:
+            aclock=time.ctime()
+            print(aclock)
+            speak(aclock)
+        if "camera" in query:
 
-
-
-with s.Microphone() as m:
-    audio=sr.listen(m)
-    query=sr.recognize_google(audio,language='eng-in')
-    print(query)
-
-if 'Discord' in query:
-    print('opening discord for YOU')
-    codepath= 'C:\\Users\\ADMIN\\Desktop\\ramya shah\\Discord.lnk'
-    os.startfile(codepath)
-if "joke" in query:
-    print(pyjokes.get_joke())
-    speak(pyjokes.get_joke())
-if "time" in query:
-    aclock=time.ctime()
-    print(aclock)
-    speak(aclock)
-if "camera" in query:
-
-    cam = cv2.VideoCapture(0)
-    while cam.isOpened():
-        ret, frame = cam.read() 
-        if cv2.waitKey(10) == ord('q'):
-            break
-        cv2.imshow('ramya shah', frame)
-if "music" in query:
-    speak('NOW PLAYING dheeme dheeme for kiara topa')
-    playsound('C:\\Users\\ADMIN\\Desktop\\python\\chapter 1\\Dheeme Dheeme.mp3')
-# elif "google" or "Google" in query:
-#     webbrowser.open('https://www.google.com/')
-elif "youtube" or "Youtube" or "YouTube" in query:
-    webbrowser.open('https://www.youtube.com/')
-elif "snake water gun" or  "Snake water gun" in query:
-    def game(comp,you):
-        if comp==you:
-            return None 
-        elif comp == 's':
-            if you=='w':
-                return False
-            elif you=='g':
-                return True
-        elif comp == 'w':
-            if you=='g':
-                return False
-            elif you=='s':
-                return True
-        elif comp == 'g':
-            if you=='s':
-                return False
-            elif you=='w':
-                return True
-    print("computer Turn: Snake(s) Water(w) or Gun(g)?")
-    randno= random.randint(1,3) 
-    if randno == 1:
-        comp = 's'
-    elif randno == 2:
-        comp = 'w'
-    elif randno == 3:
-        comp = 'g'
-
-    you = input("Your Turn: Snake(s) Water(w) or Gun(g)?")
-    print(f'you chose {you}')
-    print(f'computer chose {comp}')
-    a=game(comp,you)
-    if a== None:
-        print('THE GAME IS TIE')
-    elif a:
-        print('YOU WIN')
-    else:
-        print('YOU LOSE ')
+            cam = cv2.VideoCapture(0)
+            while cam.isOpened():
+                ret, frame = cam.read() 
+                if cv2.waitKey(10) == ord('q'):
+                    break
+                cv2.imshow('ramya shah', frame)
+        if "music" in query:
+            speak('NOW PLAYING dheeme dheeme FOR you')
+            playsound('C:\\Users\\ADMIN\\Desktop\\python\\chapter 1\\Dheeme Dheeme.mp3')
+        if query==('Google'):
+            speak("opening google for u")
+            webbrowser.open('https://www.google.com/')
+        # if "Remind" or "remind" in query:
+        #     speak('what u want to remind me please tell')
+        #     s.Microphone(query)
+        #     print(query)
+        #     with open('remind.txt',"w") as f:
+        #         f.write('queryremind')
+        #     with open('remind.txt') as re:   
+        #         reminder=re.read()
+        # if "what did i tell you" in query:
+        #     speak(reminder)
